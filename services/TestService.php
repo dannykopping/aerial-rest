44d0d2c271608ddfbead0a93c9a247bfd936543f
Danny Kopping <dannykopping@gmail.com>
2012-09-30 21:32:43 +0200

    Updated test service

--- services/TestService.php
+++ services/TestService.php
@@ -5,33 +5,14 @@
     class TestService
     {
         /**
-         * @url         /test/:includeSiblings
+         * @url         /test
          * @methods     GET,POST
          */
         public function sayHello(Request $req, Response $response)
         {
-            $rex = new stdClass();
-            $rex->name = "rex";
-            $rex->age = 28;
-            $rex->height = 1.87;
-            $rex->male = true; // questionable
-            $rex->dead = false; // questionable
-            $rex->parents = array("Avril", "Norman");
-
-            $robyn = new stdClass();
-            $robyn->name = "robyn";
-            $robyn->age = 28;
-            $robyn->height = 1.47;
-            $robyn->male = false; // questionable
-            $robyn->dead = false; // questionable
-            $robyn->parents = array("Avril", "Norman");
-
-            if($req && isset($req->params["includeSiblings"]) && $req->params["includeSiblings"] == "true")
-                $rex->siblings = array($robyn);
-
 			$response->status = Status::OK;
 			$response->headers['X-Powered-By'] = "Aerial REST";
 
-            return $rex;
+            return "Hello :)";
         }
     }
